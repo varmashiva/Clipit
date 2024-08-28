b82d474c61a38ff15303012acc0ea2538fea7674
“Rahul” <chaamrthirahul2105@gmail.com>
2024-08-28 18:04:26 +0530

    implemeted record function but not completed

--- Client/src/Components/RecordingPage.jsx
+++ Client/src/Components/RecordingPage.jsx
@@ -1,15 +1,69 @@
-import record from "../assets/facetime-button.png";
-import play from "../assets/play-button.png";
-import pause from "../assets/pause-button.png";
-import stop from "../assets/stop-button.png";
-import back from "../assets/left-arrow.png"
-import { Link } from "react-router-dom";
+// import record from "../assets/facetime-button.png";
+// import play from "../assets/play-button.png";
+// import pause from "../assets/pause-button.png";
+// import stop from "../assets/stop-button.png";
+// import back from "../assets/left-arrow.png"
+// import { Link } from "react-router-dom";
+import { useRef } from "react";
 import "../Components/RecordingCSS.css";
+import { ReactMediaRecorder } from "react-media-recorder";
 
 function RecordingPage() {
+  const stream = useRef();
+  function rec() {
+    stream.current.style.display = "block";
+
+    window.navigator.mediaDevices
+      .getUserMedia({ video: true, audio: false })
+      .then((strem) => {
+        stream.current.srcObject = strem;
+      });
+  }
+  function scre() {
+    navigator.mediaDevices.getDisplayMedia({ video: true }).then((strm) => {
+      stream.current.srcObject = strm;
+    });
+  }
+  function stop() {
+    window.navigator.mediaDevices
+      .getUserMedia({ video: false, audio: false })
+      .then((strem) => {
+        stream.current.srcObject = strem;
+      });
+    stream.current.style.display = "none";
+  }
   return (
     <div>
-      <div className="back-btn-container">
+      <ReactMediaRecorder
+        video
+        audio
+        screen
+        render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
+          <div>
+            <p>{status}</p>
+            <button
+              onClick={() => {
+                rec();
+                startRecording();
+              }}
+            >
+              Start Recording
+            </button>
+            <button
+              onClick={() => {
+                stopRecording();
+                stop();
+              }}
+            >
+              Stop Recording
+            </button>
+            <button onClick={scre}>Share Screen</button>
+            <video src={mediaBlobUrl} controls autoPlay playsInline />
+          </div>
+        )}
+      />
+      <video autoPlay ref={stream} width="50%"/>
+      {/* <div className="back-btn-container">
         <Link to="/">
           <img id="back-btn-img" src={back} alt="" />
         </Link>
@@ -38,7 +92,7 @@ function RecordingPage() {
             <img className="btnImage" src={stop} alt="" />
           </button>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
